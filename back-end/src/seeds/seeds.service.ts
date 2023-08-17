1dec7c387c91ef48c51c226d55057c6283d0d36d
Pedro Pereira <pedro.2108@hotmail.com>
2023-08-17 02:21:37 -0300

    feat: db populate script implemented

--- back-end/src/seeds/seeds.service.ts
+++ back-end/src/seeds/seeds.service.ts
@@ -1,19 +1,24 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { AxiosResponse } from 'axios';
-import mongoose from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Command, CommandRunner } from 'nest-commander';
 import { Launch } from 'src/launches/models/launch.model';
 import { Rocket } from 'src/launches/models/rocket.model';
 
-@Injectable()
 @Command({
   name: 'seeds',
   description: 'Run seeds to populate db with rockets and launches info',
   options: { isDefault: true },
 })
+@Injectable()
 export class SeedsService extends CommandRunner {
-  constructor(private readonly httpService: HttpService) {
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Launch.name) private launchModel: Model<Launch>,
+    @InjectModel(Rocket.name) private rocketModel: Model<Rocket>,
+  ) {
     super();
   }
 
@@ -21,10 +26,24 @@ export class SeedsService extends CommandRunner {
     const launches = await this.getLaunches();
     const rockets = await this.getRockets();
 
-    console.log({
-      launches: this.handleLaunchData(launches),
-      rockets: this.handleRocketData(rockets),
-    });
+    const mappedSeedsToCreate = [
+      { repository: this.launchModel, data: this.handleLaunchData(launches) },
+      { repository: this.rocketModel, data: this.handleRocketData(rockets) },
+    ];
+
+    await Promise.all(
+      mappedSeedsToCreate.map(async ({ data, repository }) => {
+        const totalDocs = await repository.estimatedDocumentCount();
+
+        if (totalDocs === 0) {
+          await (repository as Model<any>)
+            .insertMany(data)
+            .catch((err) => console.error(err));
+        }
+      }),
+    );
+
+    return;
   }
 
   handleLaunchData(launches) {
@@ -32,12 +51,12 @@ export class SeedsService extends CommandRunner {
       (acc, launch) => [
         ...acc,
         {
-          launchId: launch.id,
+          _id: new mongoose.mongo.ObjectId(launch.id),
           flightNumber: launch.flight_number,
           logo: launch.links.patch.small,
           missionName: launch.name,
           dateUtc: launch.date_utc,
-          rocket: new mongoose.mongo.ObjectId(launch.rocket),
+          rocketId: new mongoose.mongo.ObjectId(launch.rocket),
           result: launch.success,
           webcast: launch.links.webcast,
           isReused: launch.cores[0].reused,
@@ -53,8 +72,8 @@ export class SeedsService extends CommandRunner {
       (acc, rocket) => [
         ...acc,
         {
+          _id: new mongoose.mongo.ObjectId(rocket.id),
           name: rocket.name,
-          rocketId: new mongoose.mongo.ObjectId(rocket.id),
         },
       ],
       [],
