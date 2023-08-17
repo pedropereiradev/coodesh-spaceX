f6bfb3c162b569d042ed7f2744ba4e0a338eb314
Pedro Pereira <pedro.2108@hotmail.com>
2023-08-17 02:27:06 -0300

    fix: unnused code removed

--- back-end/src/launches/launches.controller.ts
+++ back-end/src/launches/launches.controller.ts
@@ -1,47 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { LaunchesService } from './launches.service';
-// import { CreateLaunchDto } from './dto/create-launch.dto';
-import { UpdateLaunchDto } from './dto/update-launch.dto';
 import { PaginationQueryDto } from 'src/utils/pagination/pagination-query.dto';
 
-@Controller('launches')
+@Controller('launch')
 export class LaunchesController {
-  constructor(private readonly launchesService: LaunchesService) {}
-
-  // @Post()
-  // create(@Body() createLaunchDto: CreateLaunchDto) {
-  //   return this.launchesService.create(createLaunchDto);
-  // }
+  constructor(private readonly launchService: LaunchesService) {}
 
   @Get()
   async findAll(@Query() paginationData: PaginationQueryDto) {
-    return this.launchesService.findAll(paginationData);
+    console.log({ paginationData });
+
+    return this.launchService.findAll(paginationData);
   }
-
-  // @Get(':id')
-  // async findOne(@Param('id') id: string) {
-  //   return this.launchesService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // async update(
-  //   @Param('id') id: string,
-  //   @Body() updateLaunchDto: UpdateLaunchDto,
-  // ) {
-  //   return this.launchesService.update(+id, updateLaunchDto);
-  // }
-
-  // @Delete(':id')
-  // async remove(@Param('id') id: string) {
-  //   return this.launchesService.remove(+id);
-  // }
 }
