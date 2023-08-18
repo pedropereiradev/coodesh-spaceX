8555b349a68e7b32502abe346d1d0c8e1b3d14fd
Pedro Pereira <pedro.2108@hotmail.com>
2023-08-18 15:05:08 -0300

    feat: header and footer implemented

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,22 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { RocketIcon } from '@/common/icons/rocket';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'SpaceX Dashboard',
   description: 'Dashboard das missões realizadas pela Space X',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex items-center justify-center py-4 gap-2">
+          <RocketIcon />
+          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+            Space X
+          </h1>
+        </header>
+        {children}
+        <footer className="py-5 flex justify-center items-center text-gray-400">
+          Challenge Coodesh &copy;
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
